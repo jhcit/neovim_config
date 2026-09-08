b50418e2244843f945bf360d2aa6e24e4ebc9397
Hung Nguyen <hung.jynsson@jhcit.se>
2026-09-08 17:59:50 +0000

    add a install script

---
+++ install_neovim.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "1. Cleaning up any old tarball installations..."
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo rm -f /usr/local/bin/nvim
+
+echo "2. Downloading the latest stable Neovim tarball..."
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+
+echo "3. Extracting files to /opt..."
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+echo "4. Creating a clean system symlink..."
+sudo ln -s /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+
+echo "5. Cleaning up local archive file..."
+rm nvim-linux-x86_64.tar.gz
+
+echo "6. Verifying installation:"
+nvim --version
+
+echo "Success! Neovim tarball installed perfectly."
+
